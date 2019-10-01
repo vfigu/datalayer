3adb2f42997c49fd09913d7dcd112485a6e472ea
vfigu <vicente.figueroa.950@my.csun.edu>
2019-10-01 05:51:10 -0700

    Updated Wear MainActivity.

--- wear/src/main/java/com/vfigu/datalayer/MainActivity.java
+++ wear/src/main/java/com/vfigu/datalayer/MainActivity.java
@@ -1,21 +1,132 @@
 package com.vfigu.datalayer;
 
-import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import android.widget.TextView;
+import android.content.BroadcastReceiver;
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.View;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.Node;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private TextView textView;
+    Button talkButton;
+    int receivedMessageNumber = 1;
+    int sentMessageNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView =  findViewById(R.id.text);
+        talkButton =  findViewById(R.id.talkClick);
 
-        mTextView = (TextView) findViewById(R.id.text);
+//Create an OnClickListener//
 
-        // Enables Always-on
-        setAmbientEnabled();
+        talkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String onClickMessage = "I just sent the handheld a message " + sentMessageNumber++;
+                textView.setText(onClickMessage);
+
+//Use the same path//
+
+                String datapath = "/my_path";
+                new SendMessage(datapath, onClickMessage).start();
+
+            }
+        });
+
+//Register to receive local broadcasts, which we'll be creating in the next step//
+
+        IntentFilter newFilter = new IntentFilter(Intent.ACTION_SEND);
+        Receiver messageReceiver = new Receiver();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, newFilter);
+
+    }
+
+    public class Receiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+//Display the following when a new message is received//
+
+            String onMessageReceived = "I just received a message from the handheld " + receivedMessageNumber++;
+            textView.setText(onMessageReceived);
+
+        }
+    }
+
+    class SendMessage extends Thread {
+        String path;
+        String message;
+
+//Constructor for sending information to the Data Layer//
+
+        SendMessage(String p, String m) {
+            path = p;
+            message = m;
+        }
+
+        public void run() {
+
+//Retrieve the connected devices//
+
+            Task<List<Node>> nodeListTask =
+                    Wearable.getNodeClient(getApplicationContext()).getConnectedNodes();
+            try {
+
+//Block on a task and get the result synchronously//
+
+                List<Node> nodes = Tasks.await(nodeListTask);
+                for (Node node : nodes) {
+
+//Send the message///
+
+                    Task<Integer> sendMessageTask =
+                            Wearable.getMessageClient(MainActivity.this).sendMessage(node.getId(), path, message.getBytes());
+
+                    try {
+
+                        Integer result = Tasks.await(sendMessageTask);
+
+//Handle the errors//
+
+                    } catch (ExecutionException exception) {
+
+//TO DO//
+
+                    } catch (InterruptedException exception) {
+
+//TO DO//
+
+                    }
+
+                }
+
+            } catch (ExecutionException exception) {
+
+//TO DO//
+
+            } catch (InterruptedException exception) {
+
+//TO DO//
+
+            }
+        }
     }
 }
